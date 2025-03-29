01cc9bbf3324cc215af2d69b1160b4d6865b9b2d
anaandronikashvili393 <anaandronikashvili393@gmail.com>
2025-03-29 12:40:27 +0400

    first commit

---
+++ 1/lesson2.py
@@ -0,0 +1,10 @@
+name = "ana"
+surname = "andronikashvili"
+age = 14
+height = 159.5
+clas = 8 
+school = 157
+
+print(name + " " + str (surname) + " " + str (age) + " " + str (height) + " " + str (clas) + " " + str (school))
+
+
