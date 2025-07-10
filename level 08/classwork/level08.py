68a565d58561e0f5ec2cfffe4e1360715bee5a9a
anaandronikashvili393 <anaandronikashvili393@gmail.com>
2025-07-10 16:53:13 +0400

    second commit

---
+++ level 08/classwork/level08.py
@@ -0,0 +1,8 @@
+#კომენტარის სახით ამიხსენი რა არის control flow,
+
+#control flow - შედის: sequsen, selection, inteactio
+
+#for ციკლის დახმარებით გამოიტანე 10ჯერ HELLO
+
+for hello in range(1,11 ):
+    print("hello")
