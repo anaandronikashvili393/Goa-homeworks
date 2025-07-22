897ab1c56e8726fd417456f76d212506855da9b6
anaandronikashvili393 <anaandronikashvili393@gmail.com>
2025-07-22 22:52:29 +0400

    second commit

---
+++ level 26/classwork/level26.py
@@ -0,0 +1,46 @@
+#1)
+
+def is_positive(num):
+    if num > 6:
+       return True
+    else:
+        return False
+
+print(is_positive(3))
+
+
+
+
+#2)
+
+def max_of_two(num1,num2):
+    if num1 > num2:
+        return(max_of_two(num1,num2))
+   
+print(max_of_two(90))
+
+
+#3)
+
+def max_of_three(num):
+    if num > 3.0:
+
+
+
+
+
+
+
+
+
+
+#4)
+
+def is_hot(num1,num2):
+    if num1 < num2:
+      if 30 > 37.0:  
+        return True
+      else:
+        return False
+      
+print(is_hot())
---
+++ level 26/homework/level26.py
@@ -0,0 +1,19 @@
+# Level 26
+# 1) https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
+
+
+
+# 2) https://www.codewars.com/kata/554b4ac871d6813a03000035/train/python
+
+# 3) https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/python
+
+# 4) https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+def summation(num):
+    result = 0
+    for i in range(1,num+1):
+        result+=i
+    
+    return result
+
+
+# 5) https://www.codewars.com/kata/5264d2b162488dc400000001/train/python
