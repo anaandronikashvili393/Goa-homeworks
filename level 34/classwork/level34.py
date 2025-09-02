0cf806899c2157db9da366de0a18e49c00685fd4
anaandronikashvili393 <anaandronikashvili393@gmail.com>
2025-09-02 17:42:58 +0400

    second commit

---
+++ level 34/classwork/level34.py
@@ -0,0 +1,19 @@
+# 1) კომენტარებით ახსენით რაში გამოიყენება join და split ფუნქცია, და დაწერე join ზე და split ზე 1 მაგალითი მინიმუმ
+
+# join - ვიყენებთ რომ შევაერთოდ ლისთები
+# split - ვიყენებთ რომ სტრინგი გადავაქციოთ ლისტად 
+
+# join - 
+# numbers = ["1", "2", "3"]
+# text = " , ". join(numbers)
+# print(text)
+
+# split -
+##texts = itmen.split(" , ")
+#print(texts)
+
+
+# 2) შექმენი ფუნქცია sum_two_number რომელიც იღებს 2 არგუმენტს a და b ს და აბრუნებს მათ ჯამს
+
+def sum_two_numbers(a,b):
+    return a + b
